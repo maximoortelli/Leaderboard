a29fe6562ffa19672a263965a5b807e10802d742
maximoortelli <maximoortelli@hotmail.com>
2023-09-07 13:22:54 -0300

    Add files
    Add files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 import './style.css';
+
+import myList from './module/List.js';
+
+const populate = async () => {
+  const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IV1251GkSsG9dGnKrPGk/scores/';
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const allPlayers = await response.json();
+
+  myList(allPlayers);
+};
+
+populate();
+
+const form = document.querySelector('.theform');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const user = form.querySelector('[name="user"]').value;
+  const score = form.querySelector('[name="score"]').value;
+
+  try {
+    await fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IV1251GkSsG9dGnKrPGk/scores/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({ user, score }),
+      },
+    );
+
+    const emptyInputText = () => {
+      const user = form.querySelector('[name="user"]');
+      const score = form.querySelector('[name="score"]');
+      user.value = '';
+      score.value = '';
+    };
+
+    emptyInputText();
+  } catch (err) {
+    return err;
+  }
+  return {};
+});
+
+const refresh = document.querySelector('.button');
+refresh.addEventListener('click', async () => {
+  await populate();
+});
